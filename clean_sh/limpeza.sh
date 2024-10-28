9e4a759235be9da9366fd1f650e1bc29bd5af8d2
Gilberto Mascena <gilbertomascena@gmail.com>
2024-10-27 22:52:01 -0300

    :sparkles: feat: added comments in commands and countdown timer to 30 seconds

--- clean_sh/limpeza.sh
+++ clean_sh/limpeza.sh
@@ -1,12 +1,33 @@
+#!/bin/bash
+
+# Limpeza de pacotes
 sudo apt autoclean -y && sudo apt autoremove -y
+
+# Atualização de pacotes
 sudo apt update && sudo apt upgrade -y
+
+# Atualização de pacotes Snap
 sudo snap refresh
+
+# Limpeza da lixeira do usuário
 sudo rm -rf /home/$USER/.local/share/Trash/files/*
-sudo rm -rf /home/$USER/.local/share/Trash/files/.*
 sudo rm -rf /home/$USER/.local/share/Trash/expunged/*
 sudo rm -rf /home/$USER/.local/share/Trash/info/*
+
+# Desfragmentação do sistema de arquivos
 sudo /sbin/fstrim --all || true
+
+# Limpeza dos logs do sistema
 sudo journalctl --vacuum-time=2d
-echo "\nLimpeza e atualização concluída!"
-echo "O sistema será reiniciado em 60 segundos..."
-sudo shutdown -r 1
+
+# Contagem regressiva para reiniciar o sistema
+for i in {30..0}; do
+    clear
+    echo "Limpeza e atualização concluída!"
+    echo "O sistema será reiniciado em ${i} segundos..."
+    sleep 1
+done
+
+# Reiniciar o sistema
+sudo shutdown -r now
+

3d785037021ec5a1424bb4901816bbb89b9088ff
Gilberto Mascena <gilbertomascena@gmail.com>
2024-10-28 19:39:05 -0300

    :sparkles: feat: added script for wsl

---
+++ clean_ubuntu_wsl/limpeza_wsl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Limpeza de pacotes
+sudo apt autoclean -y && sudo apt autoremove -y
+
+# Atualização de pacotes
+sudo apt update && sudo apt upgrade -y
+
+# Atualização de pacotes Snap
+sudo snap refresh
+
+# Limpeza da lixeira do usuário
+sudo rm -rf /home/$USER/.local/share/Trash/files/*
+sudo rm -rf /home/$USER/.local/share/Trash/expunged/*
+sudo rm -rf /home/$USER/.local/share/Trash/info/*
+
+# Desfragmentação do sistema de arquivos
+sudo /sbin/fstrim --all || true
+
+# Limpeza dos logs do sistema
+sudo journalctl --vacuum-time=2d
